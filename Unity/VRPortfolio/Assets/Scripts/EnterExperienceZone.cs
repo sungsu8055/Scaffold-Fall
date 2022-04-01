b4c8f78ada6f6c71d667bf6261e0d7bd3a36b9d4
sungsu8055 <98731917+sungsu8055@users.noreply.github.com>
2022-04-01 22:41:23 +0900

    04.01 작업

--- Unity/VRPortfolio/Assets/Scripts/EnterExperienceZone.cs
+++ Unity/VRPortfolio/Assets/Scripts/EnterExperienceZone.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class EnterExperienceZone : MonoBehaviour
 {
     public GameObject zoneEntryUI;
+    public GameObject player;
+    public Transform originPos;
 
     void Start()
     {
@@ -17,8 +19,39 @@ public class EnterExperienceZone : MonoBehaviour
         
     }
 
-    void ZoneEntry()
+    public void ZoneEntry(Transform enterUI)
     {
-        zoneEntryUI.SetActive(true);
+        // 체험 소개 및 진행 유무 선택 UI 출력
+        enterUI.gameObject.SetActive(true);
+
+        // UI 내용 확인 및 집중을 위해 이동 기능 정지(초기 위치 원복을 위해 함수 호출 시 CC가 활성화 상태이면 작동하지 않음)
+        player.GetComponent<CharacterController>().enabled = false;
+    }
+
+    // 아래 Y/N 버튼 선택 기능은 추후 오큘러스 컨트롤러 터치 시 Other.gameobject.transform 값을 받아와 UI 활성 비활성 조정
+    // 위와 같이 각 구역별 UI를 따로 식별해 작동 시키기 위함
+    public void ExitZone()
+    {
+        // 초기 위치로 원복
+        player.transform.SetPositionAndRotation(originPos.position, originPos.rotation);
+        // Character Controller 활성
+        player.GetComponent<CharacterController>().enabled = true;
+        // 체험 소개 및 진행 유무 UI 비활성
+        zoneEntryUI.SetActive(false);
+        // zone 진입 기능 재활성화를 위해 idle로 변경
+        player.GetComponent<PlayerCtrl>().p_State = PlayerCtrl.PlayerState.Idle;
+
+        
+    }
+
+    public void StartExperience(Transform startPos)
+    {
+        // Button - OnClick 옵션에서 포인터한 오브젝트를 파라미터로 받아 위치 수정 시 이용
+        player.transform.SetPositionAndRotation(startPos.position, startPos.rotation);
+
+        // Character Controller 활성
+        player.GetComponent<CharacterController>().enabled = true;
+        // 체험 소개 및 진행 유무 UI 비활성
+        zoneEntryUI.SetActive(false);
     }
 }
--- Unity/VRPortfolio/Assets/Scripts/PlayerCtrl.cs
+++ Unity/VRPortfolio/Assets/Scripts/PlayerCtrl.cs
@@ -1,11 +1,11 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
     public CharacterController cc;
-    public GameObject zoneCtrl;
+    public EnterExperienceZone zoneCtrl;
 
     [Header("Control")]
     public float horizontalMove;
@@ -16,10 +16,18 @@ public class PlayerCtrl : MonoBehaviour
 
     Vector3 moveDir;
 
+    public enum PlayerState
+    {
+        Idle,
+        EnterZone
+    }
+
+    public PlayerState p_State;
+
     void Start()
     {
         cc = GetComponent<CharacterController>();
-        zoneCtrl = GetComponent<EnterExperienceZone>();
+        p_State = PlayerState.Idle;
     }
 
     void Update()
@@ -29,26 +37,39 @@ public class PlayerCtrl : MonoBehaviour
         Gravity();
 
         cc.Move(moveDir * speed * Time.deltaTime);
+
+        Debug.Log(p_State);
     }
 
     void MoveKeyInput()
     {
-        // PC or VR ???????? ???? ?????? ???? ?????? ????
+        // PC or VR 이동 키 입력값 받아옴
         horizontalMove = ARAVRInput.GetAxis("Horizontal");
         verticalMove = ARAVRInput.GetAxis("Vertical");
 
         moveDir = new Vector3(horizontalMove, 0, verticalMove);
 
-        // ?????? ???? ?????? ???? ???? ???? ????
+        // 카메라 움직임에 따라 Player Obj 방향 전환, 움직임 방향도 변경됨
         moveDir = Camera.main.transform.TransformDirection(moveDir);
     }
 
+    // 중력 작용 함수
     void Gravity()
     {
         yValocity = gravity * Time.deltaTime;
-
         moveDir.y = yValocity;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        // ExperienceZone 태그 감지 시 함수 호출
+        if (other.gameObject.CompareTag("ExperienceZone") && p_State == PlayerState.Idle)
+        {
+            // 충돌 감지 후 ZoneEntryController의 함수 호출
+            zoneCtrl.ZoneEntry(other.gameObject.transform.GetChild(0));
 
+            // 중복 활성 방지를 위해 Zone 진입 상태로 변경
+            p_State = PlayerState.EnterZone;
+        }      
+    }
 }
