05141c1d61b6904f38e324edbb147342266737a5
sungsu8055 <98731917+sungsu8055@users.noreply.github.com>
2022-04-01 17:03:33 +0900

    04.01  오전 타임 작업

---
+++ Unity/VRPortfolio/Assets/Scripts/EnterExperienceZone.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterExperienceZone : MonoBehaviour
+{
+    public GameObject zoneEntryUI;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void ZoneEntry()
+    {
+        zoneEntryUI.SetActive(true);
+    }
+}
