c44ef340308838e79a2a824570ef31ce864a069f
sungsu8055 <98731917+sungsu8055@users.noreply.github.com>
2022-05-10 10:38:11 +0900

    05.10 보고서 마무리

--- Unity/VRPortfolio/Assets/Scripts/Step3RemoveCast/EquippedSafetyBelt.cs
+++ Unity/VRPortfolio/Assets/Scripts/Step3RemoveCast/EquippedSafetyBelt.cs
@@ -24,9 +24,12 @@ public class EquippedSafetyBelt : MonoBehaviour
 
     public void StartSafetyBeltFix()
     {
+        // 안전대 결착 안내 UI와 음성 출력
         fixInstructionUI.SetActive(true);
         audioManager.PlayAudioOnce(fixSafetyGear);
 
+        // 안전대 결착 위치 가이드 출력 및
+        // 기존 컨트롤러 비활성 후 안전대 결착 컨트롤러 활성
         safetyBeltGuide.SetActive(true);
         originController.SetActive(false);
         safetyBeltController.SetActive(true);
@@ -39,11 +42,14 @@ public class EquippedSafetyBelt : MonoBehaviour
 
     IEnumerator SafetyBeltFixed(Collider collider)
     {
+        // 오른손 컨트롤러 감지 시 안전대 결착 완료 프로세스 실행
         if (collider.CompareTag("ControllerRight"))
         {
+            // 안전대 결착 안내 UI 비활성 후 결착 사운드 출력
             fixInstructionUI.SetActive(false);
             audioManager.PlayAudioOnce(fixedSound);
 
+            // 컨트롤러 모델 원복 후 안전대 모델 출력
             safetyBeltController.SetActive(false);
             originController.SetActive(true);
             safetyBeltGuide.SetActive(false);
@@ -51,6 +57,7 @@ public class EquippedSafetyBelt : MonoBehaviour
 
             yield return new WaitForSeconds(1.5f);
 
+            // 거푸집 제거 프로세스 시작
             RC.StartInstruction();
         }
     }
